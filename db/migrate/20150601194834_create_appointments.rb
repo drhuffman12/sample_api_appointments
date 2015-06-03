455bf8b1eb25fddd7521ab986d8727c3e30534b6
Daniel Huffman <drhuffman12@yahoo.com>
2015-06-03 00:04:36 -0400

    UI mods and datetime defaults

--- db/migrate/20150601194834_create_appointments.rb
+++ db/migrate/20150601194834_create_appointments.rb
@@ -1,8 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.datetime :start_time
-      t.datetime :end_time
+      t.datetime :start_time, :default => Time.now.end_of_hour + 1 + 30.minutes
+      t.datetime :end_time, :default => Time.now.end_of_hour + 1 + 60.minutes
       t.string :first_name
       t.string :last_name
       t.text :comments
